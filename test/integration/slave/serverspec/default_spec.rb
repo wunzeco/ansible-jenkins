f1f517559d59d68aa637ce80d2c9b962a8db62c3
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2016-10-31 13:05:00 +0000

    [OI] add support for centos 7 jenkins-slave

--- test/integration/slave/serverspec/default_spec.rb
+++ test/integration/slave/serverspec/default_spec.rb
@@ -4,8 +4,15 @@ jenkins_slave_user = 'jenkins-slave'
 jenkins_slave_group = jenkins_slave_user
 
 
-describe package('daemon') do
-  it { should be_installed }
+jenkins_slave_startup_script_config  = '/etc/default/jenkins-slave'
+if os[:family] =~ /centos|redhat/
+  jenkins_slave_startup_script_config  = '/etc/sysconfig/jenkins-slave'
+end
+
+if os[:family] =~ /debian|ubuntu/
+  describe package('daemon') do
+    it { should be_installed }
+  end
 end
 
 describe group(jenkins_slave_group) do
@@ -23,7 +30,7 @@ describe file('/home/jenkins-slave') do
   it { should be_mode 755 }
 end
 
-describe file('/etc/default/jenkins-slave') do
+describe file(jenkins_slave_startup_script_config) do
   it { should be_file }
   it { should be_owned_by 'root' }
   it { should be_mode 644 }
