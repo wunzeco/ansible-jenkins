2ee02a2a70c4d5cedd1e84a4acc4ddb0c1547f36
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2016-10-31 12:38:10 +0000

    [OI] remove google-chrome install tasks now moved to a new ansible role

--- test/integration/master/serverspec/default_spec.rb
+++ test/integration/master/serverspec/default_spec.rb
@@ -86,22 +86,3 @@ end
 # jenkins ui users
 ###################
 # TODO
-
-
-#################
-# google-chrome
-#################
-
-#describe package('google-chrome-stable') do
-#  it { should be_installed }
-#end
-#
-#describe file('/usr/local/share/chromedriver') do
-#  it { should be_file }
-#  it { should be_mode 755 }
-#end
-#
-#describe file('/usr/bin/chromedriver') do
-#  it { should exist }
-#  it { should be_symlink }
-#end
